2411c3ca739f2407f07938ad219b6a39b8e236c1
shweousan <shweousann@gmail.com>
2026-09-09 09:20:08 -0800

    first commit

---
+++ src/main/java/com/mycompany/githubfirsttuto/GitHubFirstTuto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubfirsttuto;
+
+/**
+ *
+ * @author Admin
+ */
+public class GitHubFirstTuto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
